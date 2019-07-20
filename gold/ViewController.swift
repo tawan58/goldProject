fb242f16430730cc23438a3a541b325e689ebedb
Tawan <tawanrungk58@email.nu.ac.th>
2019-07-20 14:34:25 +0700

    Initial Commit

---
+++ gold/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gold
+//
+//  Created by Admin on 20/7/2562 BE.
+//  Copyright © 2562 Kate. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
